c3aa77ca3959d5c6f8758fc7dd5894eec47a2f36
Supraja Devasani <suprajadevasani@outlook.com>
2019-05-21 21:28:33 +0530

    sql Added

---
+++ WorkOutBlank/WorkOutContext.sql
@@ -0,0 +1,39 @@
+﻿use DbProject
+Create table workout_Category
+(
+category_id int,
+Name Varchar(64)
+constraint pk_workout_category primary key(category_id)
+
+)
+
+Create table workout_Collection
+(
+  workout_title  Varchar(128),
+  workout_note varchar(256),
+  calories_burn_per_min float,
+   category_id  int,
+	workout_id int,
+	constraint pk_workout_Collection primary key(workout_id),
+	constraint fk_workout_Collection_workout_category foreign key(category_id) references workout_category(category_id)
+	)
+
+Create table workout_active
+(
+    workout_id int,
+    start_time time,
+     start_date date,
+    end_date date,
+    end_time time,
+    comment varchar(64),
+    status bit,
+	constraint fk_workout_active_workout_Collection foreign key(workout_id) references workout_Collection(workout_id),
+	names varchar(50) primary key
+	)
+	
+
+
+  
+  select * from workout_Category
+   select * from workout_Collection
+     select * from workout_active
